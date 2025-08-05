e0a664f695d4d7873ad813ac988eca1331f73934
DelioCoder <davideveriwhere@gmail.com>
2025-08-05 12:59:40 -0500

    Auth functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,36 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
-import { RegisterUserDto } from './dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt'
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { User } from './entity/user.entity';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { envs } from 'src/config';
 
 @Injectable()
 export class AuthService {
 
-    private readonly logger = new Logger('AuthService');
-
     constructor(
         @InjectModel(User.name)
-        private readonly userModel: Model<User>
+        private readonly userModel: Model<User>,
+        private readonly jwtService: JwtService
     ) { }
+
+    async signJwt( payload: JwtPayload ) {
+        return this.jwtService.sign(payload);
+    }
 
     async registerUser(registerUserDto: RegisterUserDto) {
 
         const { email, name, password } = registerUserDto;
 
         try {
-            
-            const user = await this.userModel.findOne({name});
 
-            if ( user ) {
+            const user = await this.userModel.findOne({ name });
+
+            if (user) {
                 throw new RpcException({
                     status: 400,
                     message: 'User already exist'
@@ -33,18 +40,85 @@ export class AuthService {
             const newUser = await this.userModel.create({
                 name,
                 email,
-                password
+                password: bcrypt.hashSync(password, 10)
             });
 
+            const returnedUser = { id: newUser.id, email: newUser.email, name: newUser.name };
+
             return {
-                user: newUser,
-                token: 'ABC'
+                user: returnedUser,
+                token: await this.signJwt( returnedUser ),
             }
 
         } catch (ex) {
-            throw new RpcException({ex})
+            this.handleDBException(ex);
         }
 
+    }
+
+    async loginUser(loginUserDto: LoginUserDto) {
+
+        const { email, password } = loginUserDto;
+
+        try {
+
+            const user = await this.userModel.findOne({ email });
+
+            if (!user) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'Invalid credentials'
+                });
+            }
+
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+            if (!isPasswordValid) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'User/Password not valid'
+                })
+            }
+
+            const returnedUser = { id: user.id, email: user.email, name: user.name };
+
+            return {
+                user: returnedUser,
+                token: await this.signJwt( returnedUser ),
+            }
+
+        } catch (ex) {
+            this.handleDBException(ex);
+        }
+
+    }
+
+    async verifyToken(token: string) {
+        try {
+            const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+                secret:envs.JWT_TOKEN
+            });
+
+            return {
+                user: user,
+                token: await this.signJwt(user)
+            }
+            
+        } catch (ex) {
+            throw new RpcException({
+                status: 401,
+                message: 'Invalid Token'
+            })
+        }
+    }
+
+    private handleDBException(ex: any) {
+        if (ex instanceof RpcException) throw ex;
+
+        throw new RpcException({
+            status: 500,
+            message: ex?.message || 'Internal server error',
+        });
     }
 
 }
