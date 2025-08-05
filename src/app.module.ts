bef227aa5c1acc5bdd1cd63c543315206da9dacd
DelioCoder <davideveriwhere@gmail.com>
2025-08-04 22:01:50 -0500

    user-entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { envs } from './config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(envs.DATABASE_URL)
+  ],
   controllers: [],
   providers: [],
 })
