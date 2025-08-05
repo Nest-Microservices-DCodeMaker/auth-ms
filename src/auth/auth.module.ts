636c296baed2e0bdc19c190bf0ca040226ca1f3e
DelioCoder <davideveriwhere@gmail.com>
2025-08-05 12:59:11 -0500

    JwtModule configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entity/user.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from 'src/config';
 
 @Module({
   controllers: [AuthController],
@@ -15,7 +17,12 @@ import { AuthController } from './auth.controller';
         name: User.name,
         schema: UserSchema
       }
-    ])
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: envs.JWT_TOKEN,
+      signOptions: { expiresIn: '1h' }
+    })
   ]
 })
 export class AuthModule {}
