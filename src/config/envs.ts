1200216344414044b0172b791e722facbfd84319
DelioCoder <davideveriwhere@gmail.com>
2025-08-05 12:58:01 -0500

    JwtSecret variable added

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,26 +5,28 @@ import * as joi from 'joi';
 interface EnvVars {
     NATS_SERVERS: string;
     DATABASE_URL: string;
+    JWT_SECRET: string;
 }
 
 const envsSchema = joi.object({
-    NATS_SERVERS: joi.array().items( joi.string() ).required(),
-    DATABASE_URL: joi.string().required()
-})
-.unknown(true)
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
+    DATABASE_URL: joi.string().required(),
+    JWT_SECRET: joi.string().required()
+}).unknown(true)
 
 const { error, value } = envsSchema.validate({
     ...process.env,
     NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
 });
 
-if ( error ) {
-    throw new Error(`Config validation error: ${ error.message }`)
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
 }
 
 const envVars: EnvVars = value;
 
 export const envs = {
     NATS_SERVERS: envVars.NATS_SERVERS,
-    DATABASE_URL: envVars.DATABASE_URL
+    DATABASE_URL: envVars.DATABASE_URL,
+    JWT_TOKEN: envVars.JWT_SECRET,
 }
