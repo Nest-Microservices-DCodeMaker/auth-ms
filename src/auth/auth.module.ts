34d2d1bce76c203a5d9362f4918e61e7293db38a
DelioCoder <davideveriwhere@gmail.com>
2025-08-04 22:02:23 -0500

    MongooseModule imported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { NatsModule } from 'src/transports/nats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entity/user.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [NatsModule]
+  imports: [
+    NatsModule,
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ])
+  ]
 })
 export class AuthModule {}
